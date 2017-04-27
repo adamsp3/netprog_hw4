3c0aa800cf30957276cc671bbb43647af566ec82
perriadams <adamsp3@rpi.edu>
2017-04-27 02:13:40 -0400

    Fixed the client server_addr issues

--- server.c
+++ server.c
@@ -44,9 +44,10 @@ int main(int argc, char**argv)
 	if(argc < 3)
 	{
 		printf("too few arguments\n");
-		exit( EXIT_FAILURE );
+		exit(EXIT_FAILURE);
 
 	}
+  
 	if(strcmp(argv[1], "server") == 0)
 	{
 		server(atoi(argv[2]));
@@ -60,7 +61,7 @@ int main(int argc, char**argv)
 		printf("inputs must be either \"server\" or \"client\" \n");
 	}
     
-    return 0;
+  return 0;
 }
 
 
@@ -74,14 +75,14 @@ void die(const char *msg)
 void server(int port)
 {
 	/* Create the listener socket as TCP socket */
-  	int sd = socket( PF_INET, SOCK_STREAM, 0 );
+  	int sd = socket(PF_INET, SOCK_STREAM, 0);
   	/* sd is the socket descriptor */
   	/*  and SOCK_STREAM == TCP       */
 
-  	if ( sd < 0 )
+  	if (sd < 0)
   	{
-    	perror( "socket() failed" );
-    	exit( EXIT_FAILURE );
+    	perror("socket() failed");
+    	exit(EXIT_FAILURE);
   	}
 
   	/* socket structures */
@@ -92,29 +93,29 @@ void server(int port)
 
   	/* htons() is host-to-network-short for marshalling */
   	/* Internet is "big endian"; Intel is "little endian" */
-  	server.sin_port = htons( port );
-  	int len = sizeof( server );
+  	server.sin_port = htons(port);
+  	int len = sizeof(server);
 
-  	if ( bind( sd, (struct sockaddr *)&server, len ) < 0 )
+  	if (bind(sd, (struct sockaddr *)&server, len) < 0)
   	{
-    	perror( "bind() failed" );
-    	exit( EXIT_FAILURE );
+    	perror("bind() failed");
+    	exit(EXIT_FAILURE);
   	}
 
-  	listen( sd, 5 );   /* 5 is the max number of waiting clients */
-  	printf( "PARENT: Listener bound to port %d\n", port );
+  	listen(sd, 5);   /* 5 is the max number of waiting clients */
+  	printf("PARENT: Listener bound to port %d\n", port);
 
   	struct sockaddr_in client;
-  	int fromlen = sizeof( client );
-  	printf( "server address is %s\n", inet_ntoa( server.sin_addr ) );
+  	int fromlen = sizeof(client);
+  	printf("server address is %s\n", inet_ntoa(server.sin_addr));
 
   	char buffer[ BUFFER_SIZE ];
 
-  	while ( 1 )
+  	while (1)
   	{
-    	int newsock = accept( sd, (struct sockaddr *)&client,
-                          (socklen_t*)&fromlen );
-    	printf( "PARENT: Accepted client connection\n" );
+    	int newsock = accept(sd, (struct sockaddr *)&client,
+                          (socklen_t*)&fromlen);
+    	printf("PARENT: Accepted client connection\n");
 
     	/* handle new socket in a child process,
        allowing the parent process to immediately go
@@ -127,59 +128,59 @@ void server(int port)
 void client(int port)
 {
 	/* create TCP client socket (endpoint) */
-	int sock = socket( PF_INET, SOCK_STREAM, 0 );
+	int sock = socket(PF_INET, SOCK_STREAM, 0);
 
-  	if ( sock < 0 )
+  	if (sock < 0)
   	{	
-    	perror( "socket() failed" );
-    	exit( EXIT_FAILURE );
+    	perror("socket() failed");
+    	exit(EXIT_FAILURE);
   	}
 
-  	struct hostent * hp = gethostbyname( "linux00.cs.rpi.edu" );
-  	if ( hp == NULL )
+  	struct hostent * hp = gethostbyname("0.0.0.0");
+  	if (hp == NULL)
   	{
-    	perror( "gethostbyname() failed" );
-    	exit( EXIT_FAILURE );
+    	perror("gethostbyname() failed");
+    	exit(EXIT_FAILURE);
   	}
 
   	struct sockaddr_in server;
   	server.sin_family = PF_INET;
-  	memcpy( (void *)&server.sin_addr, (void *)hp->h_addr,
-          hp->h_length );
-  	server.sin_port = htons( port );
+  	memcpy((void *)&server.sin_addr, (void *)hp->h_addr,
+          hp->h_length);
+  	server.sin_port = htons(port);
 
-  	printf( "server address is %s\n", inet_ntoa( server.sin_addr ) );
+  	printf("server address is %s\n", inet_ntoa(server.sin_addr));
 
-  	if ( connect( sock, (struct sockaddr *)&server,
-                sizeof( server ) ) < 0 )
+  	if (connect(sock, (struct sockaddr *)&server,
+                sizeof(server)) < 0)
   	{
-    	perror( "connect() failed" );
-    	exit( EXIT_FAILURE );
+    	perror("connect() failed");
+    	exit(EXIT_FAILURE);
   	}
 
   	char * msg = "hello world";
-  	int n = write( sock, msg, strlen( msg ) );
-	fflush( NULL );
-  	if ( n < strlen( msg ) )
+  	int n = write(sock, msg, strlen(msg));
+	  fflush(NULL);
+  	if (n < strlen(msg))
   	{
-    	perror( "write() failed" );
-    	exit( EXIT_FAILURE );
+    	perror("write() failed");
+    	exit(EXIT_FAILURE);
   	}
 
   	char buffer[ BUFFER_SIZE ];
-  	n = read( sock, buffer, BUFFER_SIZE );  // BLOCK
-  	if ( n < 0 )
-	{
-    	perror( "read() failed" );
-	    exit( EXIT_FAILURE );
+  	n = read(sock, buffer, BUFFER_SIZE);  // BLOCK
+  	if (n < 0)
+	  {
+    	perror("read() failed");
+	    exit(EXIT_FAILURE);
   	}
   	else
   	{
     	buffer[n] = '\0';
-    	printf( "Received message from server: %s\n", buffer );
+    	printf("Received message from server: %s\n", buffer);
   	}
 
-  close( sock );
+  close(sock);
 
 }
 
